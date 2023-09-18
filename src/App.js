4c46789e0a2bef09132ef9a10361a114b04ba8a0
mmedin <miguel.e.medina@gmail.com>
2023-09-18 17:54:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React BBVA amigos de Junio</h1>
+          <h1 className="App-title">Welcome to React BBVA amigos de Septiembre</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
